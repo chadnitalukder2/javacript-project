0889e44b30af3637023170b772c96fa87a24fa66
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-10-09 21:00:49 +0600

    problem

---
+++ problem sollve-1/index.js
@@ -0,0 +1,15 @@
+//problem-1  ludu khelar guti 1-6 kivabe ashe
+/*function getRndInteger(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) ) + min;
+  }
+  console.log(getRndInteger(1,6));*/
+  
+  //problem-2  kivabe alpabet onujaiye shajanu jay
+/*const fruits = ["Banana", "Orange", "Apple", "Mango"];
+fruits.sort();
+console.log(fruits);*/
+
+//problem-3
+/*const points = [40, 100, 1, 5, 25, 10];
+points.sort(function(a, b){return a - b});
+console.log(points);*/
