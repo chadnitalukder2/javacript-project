7a2538d911cf3f763566d2915e8285e6c733c134
Puja <137037747+chadnitalukder2@users.noreply.github.com>
2023-11-24 00:40:21 +0600

    Speech Generator

--- Speech Generator/script.js
+++ Speech Generator/script.js
@@ -1,5 +1,28 @@
 let speech = new SpeechSynthesisUtterance();
 
+let voices = [];
+
+let voiceSelect = document.querySelector("select");
+
+window.speechSynthesis.onvoiceschanged = () => {
+    voices = window.speechSynthesis.getVoices();
+
+    // Check if voices are available
+    if (voices.length > 0) {
+        speech.voice = voices[0];
+
+        voices.forEach((voice, i) => (voiceSelect.options[i] = new Option(voice.name, i)));
+    }
+};
+
+// Manually trigger the event
+window.speechSynthesis.onvoiceschanged();
+
+voiceSelect.addEventListener("change", () =>{
+speech.voice = voices[voiceSelect.value];
+});
+
+
 document.querySelector("button").addEventListener("click", () =>{
     speech.text = document.querySelector("textarea").value;
     window.speechSynthesis.speak(speech);
